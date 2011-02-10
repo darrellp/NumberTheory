03495a22114a73a58ca8909e0a2724db8e0dced8
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-10 06:22:02 +0000

    Initial Checkin

---
+++ trunk/NumberTheory/EuclideanAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿#if BIGINTEGER
+using System.Numerics;
+using nt=System.Numerics.BigInteger;
+#elif LONG
+using nt = System.Int64;
+#endif
+
+
+namespace NumberTheory
+{
+    public static class Euclidean
+    {
+        public static nt GCD(this nt val1, nt val2)
+        {
+#if BIGINTEGER
+            return BigInteger.GreatestCommonDivisor(val1, val2);
+#endif
+            while (val2 != 0)
+            {
+                var r = val1 % val2;
+                val1 = val2;
+                val2 = r;
+            }
+            return val1;
+        }
+
+        public static EuclideanExt EuclideanExt(this nt val1, nt val2)
+        {
+            return new EuclideanExt(val1, val2);
+        }
+    }
+}
---
+++ trunk/NumberTheory/EuclideanExt.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+#if BIGINTEGER
+using nt = System.Numerics.BigInteger;
+#elif LONG
+using nt = System.Int64;
+#endif
+
+namespace NumberTheory
+{
+    public class EuclideanExt
+    {
+        public nt GCD { get; private set; }
+        public nt Coeff1 { get; private set; }
+        public nt Coeff2 { get; private set; }
+
+        internal EuclideanExt(nt val1, nt val2)
+        {
+            var rst = new[] { new[] { val1, 1, 0 }, new[] { val2, 0, 1 } };
+
+            while (rst[1][0] != 0)
+            {
+                var q = rst[0][0]/rst[1][0];
+                var rst1Save = rst[1];
+                rst[1] = rst[0].Zip(rst[1], (v1, v2) => v1 - q*v2).ToArray();
+                rst[0] = rst1Save;
+            }
+            GCD = rst[0][0];
+            Coeff1 = rst[0][1];
+            Coeff2 = rst[0][2];
+        }
+
+    }
+}
