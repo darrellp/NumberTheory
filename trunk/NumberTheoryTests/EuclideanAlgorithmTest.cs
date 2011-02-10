d28f9ca435ff2b054186aacb980c431e994906d3
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-10 08:39:11 +0000

    DiophantineSolve for Linear 2 parametric functions

--- trunk/NumberTheoryTests/EuclideanAlgorithmTest.cs
+++ trunk/NumberTheoryTests/EuclideanAlgorithmTest.cs
@@ -1,8 +1,15 @@
-﻿using System.Numerics;
+﻿#if BIGINTEGER
+using System;
+using System.Numerics;
+using nt = System.Numerics.BigInteger;
+#elif LONG
+using nt = System.Int64;
+#endif
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using NumberTheory;
-using nt = NumberTheory.Euclidean;
+using euc = NumberTheory.Euclidean;
 
 namespace NumberTheoryTests
 {
@@ -12,22 +19,34 @@ namespace NumberTheoryTests
         [TestMethod]
         public void TestGCD()
         {
-            Assert.AreEqual(1, nt.GCD(97, 18));
-            Assert.AreEqual(6, nt.GCD(102, 18));
-            Assert.AreEqual(6, nt.GCD(18, 102));
+            Assert.AreEqual(1, euc.GCD(97, 18));
+            Assert.AreEqual(6, euc.GCD(102, 18));
+            Assert.AreEqual(6, euc.GCD(18, 102));
         }
 
         [TestMethod]
         public void TestEuclideanExt()
         {
-            EuclideanExt ext = nt.EuclideanExt(97, 18);
+            var ext = euc.EuclideanExt(97, 18);
             Assert.AreEqual(1,ext.GCD);
             Assert.AreEqual(-5, ext.Coeff1);
             Assert.AreEqual(27, ext.Coeff2);
-            ext = nt.EuclideanExt(541,7919);
+            ext = euc.EuclideanExt(541, 7919);
             Assert.AreEqual(1, ext.GCD);
             Assert.AreEqual(-1010, ext.Coeff1);
             Assert.AreEqual(69, ext.Coeff2);
+        }
+
+        [TestMethod]
+        public void TestDiophantineSolve()
+        {
+            var fn = euc.DiophantineSolve(13, 51, 500);
+
+            for (var i = 1; i < 6; i++)
+            {
+                var sln = fn(i);
+                Assert.AreEqual(500, sln[0]*13 + sln[1]*51);
+            }
         }
     }
 }
