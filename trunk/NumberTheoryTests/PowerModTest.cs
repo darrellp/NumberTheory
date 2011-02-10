c91cacc6e24bdb0224b93d6b1fcb26a84edec0da
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-10 08:51:14 +0000

    Found that negative integers don't work correctly in PowerMod and put in TODO comments

--- trunk/NumberTheoryTests/PowerModTest.cs
+++ trunk/NumberTheoryTests/PowerModTest.cs
@@ -11,6 +11,8 @@ namespace NumberTheoryTests
         public void PowerModTest()
         {
             Assert.AreEqual(3115, PowerMod.Power(2357, 2357, 3599));
+            // TODO: This doesn't work for BigInteger
+            //Assert.AreEqual(5,PowerMod.Power(7,-1,17));
         }
     }
 }
