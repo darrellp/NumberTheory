c88e1289a5ef4d92790779527bf40418e8e83bea
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-10 07:30:00 +0000

    PowerMod Tests

---
+++ trunk/NumberTheoryTests/PowerModTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NumberTheory;
+
+namespace NumberTheoryTests
+{
+    [TestClass]
+    public class PowerModTests
+    {
+        [TestMethod]
+        public void PowerModTest()
+        {
+            Assert.AreEqual(3115, PowerMod.Power(2357, 2357, 3599));
+        }
+    }
+}
