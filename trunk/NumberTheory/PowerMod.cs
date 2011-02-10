178f7ce4177dfc09b3863fc9ab1edd41efffa6b1
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-10 07:29:40 +0000

    Fast PowerMod

---
+++ trunk/NumberTheory/PowerMod.cs
@@ -0,0 +1,45 @@
+﻿#if BIGINTEGER
+using System.Numerics;
+using nt = System.Numerics.BigInteger;
+#elif LONG
+using nt = System.Int64;
+#endif
+
+namespace NumberTheory
+{
+    static public class PowerMod
+    {
+        static public nt Power(nt x, nt n, nt mod)
+        {
+#if BIGINTEGER
+            return BigInteger.ModPow(x, n, mod);
+#else
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            nt mask;
+            nt res = 1;
+
+            for (mask = (~nt.MaxValue >> 1) & nt.MaxValue; (mask & n)== 0; mask >>= 1) ;
+
+            while ((mask & n) == 0)
+            {
+                mask >>= 1;
+            }
+            while (mask != 0)
+            {
+                res *= res;
+                if ((mask & n) != 0)
+                {
+                    res *= x;
+                }
+                res = res%mod;
+                mask >>= 1;
+            }
+            return res;
+#endif
+        }
+    }
+}
