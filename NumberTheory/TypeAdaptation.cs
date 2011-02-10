cbe622757216299af3e52f33542af58280d06eaa
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-10 06:22:02 +0000

    Initial Checkin

---
+++ NumberTheory/TypeAdaptation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if BIGINTEGER
+using System.Numerics;
+using nt=System.Numerics.BigInteger;
+#elif LONG
+using nt = System.Int64;
+#endif
+
+namespace NumberTheory
+{
+    public static class TypeAdaptation
+    {
+#if BIGINTEGER
+        public static BigInteger DivRem(BigInteger n1, BigInteger n2, out BigInteger rem)
+        {
+            return BigInteger.DivRem(n1, n2, out rem);
+        }
+#else
+        public static nt DivRem(nt n1, nt n2, out nt rem)
+        {
+            return Math.DivRem(n1, n2, out rem);
+        }
+#endif
+    }
+}
---
+++ NumberTheoryTests/EuclideanAlgorithmTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NumberTheory;
+using nt = NumberTheory.Euclidean;
+
+namespace NumberTheoryTests
+{
+    [TestClass]
+    public class EuclideanAlgorithmTest
+    {
+        [TestMethod]
+        public void TestGCD()
+        {
+            Assert.AreEqual(1, nt.GCD(97, 18));
+            Assert.AreEqual(6, nt.GCD(102, 18));
+            Assert.AreEqual(6, nt.GCD(18, 102));
+        }
+
+        [TestMethod]
+        public void TestEuclideanExt()
+        {
+            EuclideanExt ext = nt.EuclideanExt(97, 18);
+            Assert.AreEqual(1,ext.GCD);
+            Assert.AreEqual(-5, ext.Coeff1);
+            Assert.AreEqual(27, ext.Coeff2);
+            ext = nt.EuclideanExt(541,7919);
+            Assert.AreEqual(1, ext.GCD);
+            Assert.AreEqual(-1010, ext.Coeff1);
+            Assert.AreEqual(69, ext.Coeff2);
+        }
+    }
+}
