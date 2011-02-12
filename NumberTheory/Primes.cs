9b350c25b5c457b7704693c035e71abae1edec2f
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-12 06:35:13 +0000

    Jacobi symbol and Strong Psuedoprimes

---
+++ NumberTheory/Primes.cs
@@ -0,0 +1,57 @@
+﻿#if BIGINTEGER
+using System;
+using nt=System.Numerics.BigInteger;
+#elif LONG
+using System;
+using nt = System.Int64;
+#endif
+
+
+#if BIGINTEGER
+namespace NumberTheoryBig
+#elif LONG
+namespace NumberTheoryLong
+#endif
+{
+    public static class Primes
+    {
+        public static bool PsuedoPrime(nt p, nt b)
+        {
+            return PowerMod.Power(b, p - 1, p) == 1;
+        }
+
+        public static bool StrongPsuedoPrime(nt n, nt b)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Negative value in StrongPsuedoPrime");
+            }
+            if ((n & 1) == 0)
+            {
+                return n == 2;
+            }
+
+            nt nReduced;
+            var s = (n - 1).TwosExponent(out nReduced);
+            var bp = PowerMod.Power(b, nReduced, n);
+            if (bp == 1)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                if (bp == 1)
+                {
+                    return false;
+                }
+                if (bp == n - 1)
+                {
+                    return true;
+                }
+                bp = PowerMod.Power(bp, 2, n);
+            }
+            return false;
+        }
+    }
+}
---
+++ NumberTheory/Utilities.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+#if BIGINTEGER
+using System;
+using System.Numerics;
+using nt = System.Numerics.BigInteger;
+#elif LONG
+using System;
+using nt = System.Int64;
+#endif
+
+
+#if BIGINTEGER
+namespace NumberTheoryBig
+#elif LONG
+namespace NumberTheoryLong
+#endif
+{
+    internal static class Utilities
+    {
+        internal static int TwosExponent(this nt n, out nt m)
+        {
+            var exp = 0;
+            m = n;
+            while ((m & 1) == 0)
+            {
+                exp++;
+                m >>= 1;
+            }
+            return exp;
+        }
+
+        internal static int NegOnePower(this nt n)
+        {
+            return ((n & 1) == 0) ? 1 : -1;
+        }
+
+        internal static int NegOnePower(this int n)
+        {
+            return ((n & 1) == 0) ? 1 : -1;
+        }
+    }
+}
