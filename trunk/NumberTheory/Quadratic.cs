6c86bc5988d727e97bec59170bd4b9b00643c861
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-16 22:13:07 +0000

    Added the SqrtMod function which is essentially Tonelli's algorithm from CNT

--- trunk/NumberTheory/Quadratic.cs
+++ trunk/NumberTheory/Quadratic.cs
@@ -1,6 +1,8 @@
 ﻿#if BIGINTEGER
+using NumberTheoryBig;
 using nt = System.Numerics.BigInteger;
-#elif LONG
+#else
+using NumberTheoryLong;
 using nt = System.Int64;
 #endif
 
@@ -50,9 +52,11 @@ namespace NumberTheoryLong
 			// Is a Negative?
 			if (a < 0)
 			{
+				// Reverse sign and set our correction factor properly
 				a = -a;
 				negCorrection = ((n - 1)/2).NegOnePower();
 			}
+
 			// Use the absolute value of the "denominator"
 			n = TypeAdaptation.Abs(n);
 
@@ -79,5 +83,115 @@ namespace NumberTheoryLong
 			}
 			return negCorrection * (gcd != 1 ? 0 : c);
 		}
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Takes the square root of a mod p if one exists. </summary>
+		///
+		/// <remarks>	
+		/// <para>The out parameter, fSuccess, indicates our ability to locate a square root.</para>
+		/// <para>This algorithm is essentially the one in the Pomerance book "Prime Numbers...".  I
+		/// think that it's a teeny bit more efficient than "Tonelli's algorithm" in Wagon's CNT but
+		/// they're both the same idea.  In CNT, they only check primes when looking for non-residues.
+		/// I'm not sure why.  Pomerance makes no mention of any necessity for doing this nor do I
+		/// see one.</para>
+		/// Darrellp, 2/16/2011. 
+		/// </remarks>
+		///
+		/// <param name="a">		The number whose square root is desired </param>
+		/// <param name="p">		The prime we're doing this modulo. </param>
+		/// <param name="fSuccess">	[out] False if there is no square root. </param>
+		///
+		/// <returns>	The square root if fSuccess is true, else indeterminate. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		static public nt SqrtMod(nt a, nt p, out bool fSuccess)
+		{
+			// If there's no square root
+			if (Jacobi(a, p) != 1)
+			{
+				// Indicate that there's no square root and return
+				fSuccess = false;
+				return 0;
+			}
+
+			// Indicate that we can find a square root
+			fSuccess = true;
+
+			// If p is not congruent to 1 mod 8
+			if (p % 8 != 1)
+			{
+				// Return the simple case
+				return SqrtSimpleCase(a, p);
+			}
+
+			// Find a random non-residue
+			var d = NonResidue(p);
+
+			// Set up A, D and m for the loop
+			nt t;
+			var s = (p - 1).TwosExponent(out t);
+			var aCap = PowerMod.Power(a, t, p);
+			var dCap = PowerMod.Power(d, t, p);
+			nt m = 0;
+
+			// For each step in the halving
+			for (var iHalve = 0; iHalve < s; iHalve++ )
+			{
+				// Modify m appropriately
+				var prod = (aCap*PowerMod.Power(dCap, m, p)) << (s - 1 - iHalve);
+
+				// If our product is congruent to -1 mod p
+				if ((prod % p) == p - 1)
+				{
+					// Adjust m accordingly
+					m += ((nt) 1 << iHalve);
+				}
+			}
+
+			// Return our square root
+			return (PowerMod.Power(a, (t + 1)/2, p)*PowerMod.Power(dCap, m/2, p))%p;
+		}
+
+		private static nt NonResidue(nt p)
+		{
+			// For each possible non-residue
+			for (nt i = 2; i < p; i++)
+			{
+				// Is it, in fact, a non-residue?
+				if (Jacobi(i, p) != 1)
+				{
+					// Return it.
+					return i;
+				}
+			}
+			// We'll never get here
+			return -1;
+		}
+
+		// This routine is only called if a is not congruent to 1 mod 8.
+		private static nt SqrtSimpleCase(nt a, nt p)
+		{
+			nt x;
+
+			// if a is congruent to 5 mod 8
+			if ((a % 8) == 5)
+			{
+				x = PowerMod.Power(a, (p + 3)/8, p);
+				
+				// If the current value of x is not a square root
+				if (x * x % p != a % p)
+				{
+					// Modify it
+					x = (x * PowerMod.Power(2, (p - 1)/4, p)) % p;
+				}
+			}
+			else
+			{
+				// Simple case when a is congruent to 3 or 7 mod 8
+				x = PowerMod.Power(a, (p + 1)/4, p);
+			}
+			return x;
+		}
+
 	}
 }
