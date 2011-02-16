6de2db531f737db7af52af1bcf12cc8a32db5e66
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-16 22:13:07 +0000

    Added the SqrtMod function which is essentially Tonelli's algorithm from CNT

--- NumberTheoryTests/QuadraticTest.cs
+++ NumberTheoryTests/QuadraticTest.cs
@@ -2,24 +2,36 @@
 
 #if BIGINTEGER
 using NumberTheoryBig;
+using nt = System.Numerics.BigInteger;
 #else
 using NumberTheoryLong;
+using nt = System.Int64;
 #endif
 
 namespace NumberTheoryTests
 {
-    [TestClass]
-    public class QuadraticTests
-    {
-        [TestMethod]
-        public void TestJacobi()
-        {
-            var results = new[] {1, 0, 1, 0, 0, 0, -1, 0, 1, 0, -1, 0, 1, 0, 0, 0, -1, 0, -1, 0};
-            for (var i = 1; i <= 20; i++)
-            {
-                Assert.AreEqual(results[i - 1], Quadratic.Jacobi(10, i));
-            }
+	[TestClass]
+	public class QuadraticTests
+	{
+		[TestMethod]
+		public void TestJacobi()
+		{
+			var results = new[] {1, 0, 1, 0, 0, 0, -1, 0, 1, 0, -1, 0, 1, 0, 0, 0, -1, 0, -1, 0};
+			for (var i = 1; i <= 20; i++)
+			{
+				Assert.AreEqual(results[i - 1], Quadratic.Jacobi(10, i));
+			}
 			Assert.AreEqual(1, Quadratic.Jacobi(-7, 100000007));
-        }
-    }
+		}
+
+		[TestMethod]
+		public void TestSqrtMod()
+		{
+			bool fSuccess;
+
+			var i = Quadratic.SqrtMod(1619, 12377, out fSuccess);
+			Assert.IsTrue(fSuccess);
+			Assert.AreEqual(1619, (i*i) % 12377);
+		}
+	}
 }
