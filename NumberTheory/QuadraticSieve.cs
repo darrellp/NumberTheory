772f6621704e93a9f00a087c8ad48bbd6161d4bc
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-20 21:54:06 +0000

    Initial checkin - non-working, but compilable code

---
+++ NumberTheory/QuadraticSieve.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if BIGINTEGER
+using nt = System.Numerics.BigInteger;
+using System.Numerics;
+#elif LONG
+using nt = System.Int64;
+#endif
+
+
+#if BIGINTEGER
+namespace NumberTheoryBig
+#elif LONG
+namespace NumberTheoryLong
+#endif
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>	Class to hold the static routine implementing the quadratic sieve. </summary>
+	///
+	/// <remarks>	Darrellp, 2/16/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static class QuadraticSieve
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Factors n using a quadratic sieve</summary>
+		///
+		/// <remarks>	Darrellp, 2/16/2011. </remarks>
+		///
+		/// <param name="n">	The number to be factored. </param>
+		///
+		/// <returns>	A factor of the number if one exists. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		public static nt Factor(nt n)
+		{
+			// Verify that n is not a power
+			nt fctPower = CheckPower(n);
+
+			// If it is
+			if (fctPower > 0)
+			{
+				// Return it's root
+				return fctPower;
+			}
+
+			// Determine B for B-smooth number base
+			var b = DetermineB(n);
+
+			// Find odd primes <= B which are quadratic residues
+			var primeList = GetPrimeList(n, b);
+
+			// for each prime found, find the square root of n mod that prime
+			bool fSuccess;
+			var sqrtList = primeList.Select(p => ((nt)p).SqrtMod(n, out fSuccess)).ToList();
+
+			// Compute our Candidate list
+			var candidates = SieveCandidates(primeList, n);
+
+			// Find a set of rows that add to 0 mod 2
+
+			// Determine y from the solution found above
+			nt y = 0;
+
+			// Determine x as the product of values represented by the rows in the solution
+			nt x = 0;
+
+			// Return GCD(x - y, n) and hope for the best
+			return n.GCD(x - y);
+		}
+
+		private static nt CheckPower(nt n)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static IEnumerable<int[]> SieveCandidates(int[] primeList, nt n)
+		{
+			// Set sqrtN to the integer square root of n
+			var sqrtN = n.ISqrt() + 1;
+
+			// Sieve our values out of primeList
+			return Enumerable.
+				// Get an "infinite" range of values
+				Range(0,int.MaxValue).
+				// Offset them by the square root of n
+				Select(indx => sqrtN + indx).
+				// Factor them over our factor base
+				Select(cand => CheckBSmooth(cand*cand - n, primeList)).
+				// Toss the ones that aren't  B-smooth
+				Where(lst => lst != null).
+				// Only keep K of the rest
+				Take(primeList.Length);
+		}
+
+		private static int CountFactors( ref nt cand, int p)
+		{
+			var nCount = 0;
+			while (cand % p == 0)
+			{
+				cand /= p;
+				nCount++;
+			}
+			return nCount;
+		}
+
+		private static int[] CheckBSmooth(nt cand, int[] primeList)
+		{
+			//!+ TODO: Optimize this!
+			// Turn each prime in our factor base into the exponent for that prime in the candidate
+			var expList = primeList.
+				Select(fct => CountFactors(ref cand, fct)).
+				ToArray();
+
+			// If we whittled the candidate down to 1, then it's completely factored
+			return cand == 1 ? expList : null;
+		}
+
+		private static int[] GetPrimeList(nt n, int b)
+		{
+			// Set sp for convenience
+			var sp = Primes.SmallPrimes;
+
+			// Have we got enough primes in our small primes array?
+			if (b < sp[sp.Length - 1])
+			{
+				// Filter out the non-residues and return the rest of the primes smaller than B
+				return sp.
+					Take(Array.BinarySearch(sp, b)).
+					Where(cand=>Quadratic.Jacobi(n, cand) == 1).
+					Select(bi => (int)bi).
+					ToArray();
+			}
+			//!+TODO: Implement case where we need more primes!!!
+			throw new NotImplementedException();
+		}
+
+		private static int DetermineB(nt n)
+		{
+			// Probably too large - at least according to Pomerance
+#if BIGINTEGER
+			return (int) Math.Exp(Math.Sqrt(nt.Log(n)*Math.Log(nt.Log(n))));
+#else
+			return (int)Math.Exp(Math.Sqrt(Math.Log(n) * Math.Log(Math.Log(n))));
+#endif
+		}
+	}
+}
