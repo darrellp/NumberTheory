748043cb46662f302344c944a9685761df88505d
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-21 09:41:07 +0000

    Commenting changes

--- NumberTheory/EuclideanAlgorithm.cs
+++ NumberTheory/EuclideanAlgorithm.cs
@@ -15,235 +15,251 @@ namespace NumberTheoryBig
 namespace NumberTheoryLong
 #endif
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// <summary>   Static class to hold routines related to the Euclidean algorithm. </summary>
-    ///
-    /// <remarks>   Darrellp, 2/12/2011. </remarks>
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>   Static class to hold routines related to the Euclidean algorithm. </summary>
+	///
+	/// <remarks>   Darrellp, 2/12/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static class Euclidean
-    {
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   The standard Euclidean algorithm. </summary>
-        ///
-        /// <remarks>   Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="val1"> The first value. </param>
-        /// <param name="val2"> The second value. </param>
-        ///
-        /// <returns>   The GCD of val1 and val2. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static class Euclidean
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   The standard Euclidean algorithm. </summary>
+		///
+		/// <remarks>   Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="val1"> The first value. </param>
+		/// <param name="val2"> The second value. </param>
+		///
+		/// <returns>   The GCD of val1 and val2. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static nt GCD(this nt val1, nt val2)
-        {
+		public static nt GCD(this nt val1, nt val2)
+		{
 #if BIGINTEGER
-            return BigInteger.GreatestCommonDivisor(BigInteger.Abs(val1), BigInteger.Abs(val2));
+			// If we're working with BigIntegers, there's a library routine for this
+			return BigInteger.GreatestCommonDivisor(BigInteger.Abs(val1), BigInteger.Abs(val2));
 #else
-            val1 = Math.Abs(val1);
-            val2 = Math.Abs(val2);
+			// Use absolute values
+			val1 = Math.Abs(val1);
+			val2 = Math.Abs(val2);
 
-            while (val2 != 0)
-            {
-                var r = val1 % val2;
-                val1 = val2;
-                val2 = r;
-            }
-            return val1;
+			// While our remainder is non-zero
+			while (val2 != 0)
+			{
+				// Do another division
+				var r = val1 % val2;
+				val1 = val2;
+				val2 = r;
+			}
+			// Return the answer
+			return val1;
 #endif
-        }
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   The standard Euclidean algorithm for multiple arguments. </summary>
-        ///
-        /// <remarks>   Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="vals"> A variable-length parameters list containing vals. </param>
-        ///
-        /// <returns>   The GCD of all the values passed in. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   The standard Euclidean algorithm for multiple arguments. </summary>
+		///
+		/// <remarks>   Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="vals"> A variable-length parameters list containing vals. </param>
+		///
+		/// <returns>   The GCD of all the values passed in. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static nt GCD(params nt[] vals)
-        {
-            return vals.Aggregate(GCD);
-        }
+		public static nt GCD(params nt[] vals)
+		{
+			return vals.Aggregate(GCD);
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Least Common Multiple for multiple values. </summary>
-        ///
-        /// <remarks>   Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="vals"> A variable-length parameters list containing vals. </param>
-        ///
-        /// <returns>   The LCM of the passed in values. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Least Common Multiple for multiple values. </summary>
+		///
+		/// <remarks>   Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="vals"> A variable-length parameters list containing vals. </param>
+		///
+		/// <returns>   The LCM of the passed in values. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static nt LCM(params nt[] vals)
-        {
-            return vals.Aggregate(LCM);
-        }
+		public static nt LCM(params nt[] vals)
+		{
+			return vals.Aggregate(LCM);
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Least Common Multiple. </summary>
-        ///
-        /// <remarks>   Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="val1"> The first value. </param>
-        /// <param name="val2"> The second value. </param>
-        ///
-        /// <returns>   The LCM of the val1 and val2. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Least Common Multiple. </summary>
+		///
+		/// <remarks>   Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="val1"> The first value. </param>
+		/// <param name="val2"> The second value. </param>
+		///
+		/// <returns>   The LCM of the val1 and val2. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static nt LCM(this nt val1, nt val2)
-        {
-            return val1*val2/GCD(val1, val2);
-        }
+		public static nt LCM(this nt val1, nt val2)
+		{
+			return val1*val2/GCD(val1, val2);
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Implements the extended Euclidean algorithm. </summary>
-        ///
-        /// <remarks>   See the EuclideanExt class for the return value from this function.
-        /// Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="val1"> The first value. </param>
-        /// <param name="val2"> The second value. </param>
-        ///
-        /// <returns>   GCD of val1 and val2 along with the coefficients to linearly combine them 
-        /// to yield that GCD. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Implements the extended Euclidean algorithm. </summary>
+		///
+		/// <remarks>   See the EuclideanExt class for the return value from this function.
+		/// Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="val1"> The first value. </param>
+		/// <param name="val2"> The second value. </param>
+		///
+		/// <returns>   GCD of val1 and val2 along with the coefficients to linearly combine them 
+		/// to yield that GCD. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static EuclideanExt EuclideanExt(this nt val1, nt val2)
-        {
-            return new EuclideanExt(val1, val2);
-        }
+		public static EuclideanExt EuclideanExt(this nt val1, nt val2)
+		{
+			return new EuclideanExt(val1, val2);
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Solve the diophantine equation ax + by = c. </summary>
-        ///
-        /// <remarks>   This equation is only solvable if c divides the GCD of a and b.  In that case,
-        /// there are an infinite number of solutions.  This method returns a function which gives a different
-        /// solution for any value of it's passed in integral parameter.
-        /// Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="a">    The value of a in ax + by = c </param>
-        /// <param name="b">    The value of b in ax + by = c. </param>
-        /// <param name="c">    The value of c in ax + by = c. </param>
-        ///
-        /// <returns>   Function which returns a different solution for each different passed in integer
-        /// or null if no solution exists.</returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Solve the diophantine equation ax + by = c. </summary>
+		///
+		/// <remarks>   This equation is only solvable if c divides the GCD of a and b.  In that case,
+		/// there are an infinite number of solutions.  This method returns a function which gives a different
+		/// solution for any value of it's passed in integral parameter.
+		/// Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="a">    The value of a in ax + by = c </param>
+		/// <param name="b">    The value of b in ax + by = c. </param>
+		/// <param name="c">    The value of c in ax + by = c. </param>
+		///
+		/// <returns>   Function which returns a different solution for each different passed in integer
+		/// or null if no solution exists.</returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static Func<nt,nt[]> DiophantineSolve(nt a, nt b, nt c)
-        {
-            nt gcd;
+		public static Func<nt,nt[]> DiophantineSolve(nt a, nt b, nt c)
+		{
+			nt gcd;
 
-            return DiophantineSolve(a, b, c, out gcd);
-        }
+			return DiophantineSolve(a, b, c, out gcd);
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Solve the diophantine equation ax + by = c and return the GCD of a and b</summary>
-        ///
-        /// <remarks>   Identical to DiophantineSolve(nt a, nt b, nt c) except it also returns the GCD which
-        /// is required to be produced during the course of the algorithm.  This is a time savings where both the
-        /// solution and the GCD are required.
-        /// Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="a">    The value of a in ax + by = c </param>
-        /// <param name="b">    The value of b in ax + by = c. </param>
-        /// <param name="c">    The value of c in ax + by = c. </param>
-        /// <param name="gcd">  The returned GCD of a and b </param>
-        ///
-        ///<returns>   Function which returns a different solution for each different passed in integer or
-        /// null if no solution exists.</returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Solve the diophantine equation ax + by = c and return the GCD of a and b</summary>
+		///
+		/// <remarks>   Identical to DiophantineSolve(nt a, nt b, nt c) except it also returns the GCD which
+		/// is required to be produced during the course of the algorithm.  This is a time savings where both the
+		/// solution and the GCD are required.
+		/// Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="a">    The value of a in ax + by = c </param>
+		/// <param name="b">    The value of b in ax + by = c. </param>
+		/// <param name="c">    The value of c in ax + by = c. </param>
+		/// <param name="gcd">  The returned GCD of a and b </param>
+		///
+		///<returns>   Function which returns a different solution for each different passed in integer or
+		/// null if no solution exists.</returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static Func<nt, nt[]> DiophantineSolve(nt a, nt b, nt c, out nt gcd)
-        {
-            var ext = new EuclideanExt(a, b);
-            gcd = ext.GCD;
+		public static Func<nt, nt[]> DiophantineSolve(nt a, nt b, nt c, out nt gcd)
+		{
+			// Do the extended Euclidean algorithm on a and b
+			var ext = new EuclideanExt(a, b);
+			gcd = ext.GCD;
 
-            if (c % gcd != 0)
-            {
-                return null;
-            }
+			// If c doesn't divide the GCD of a and b, there's no hope
+			if (c % gcd != 0)
+			{
+				return null;
+			}
 
-            var cnst1 = c * ext.Coeff1 / gcd;
-            var cnst2 = c * ext.Coeff2 / gcd;
-            var cf1 = b / gcd;
-            var cf2 = -a / gcd;
-            nt q;
+			// Get a solution
+			var cnst1 = c * ext.Coeff1 / gcd;
+			var cnst2 = c * ext.Coeff2 / gcd;
+			var cf1 = b / gcd;
+			var cf2 = -a / gcd;
+			nt q;
 
-            if (TypeAdaptation.Abs(cnst1) > TypeAdaptation.Abs(cnst2))
-            {
-                q = cnst1 / cf1;
-            }
-            else
-            {
-                q = cnst2 / cf2;
-            }
-            cnst1 -= q * cf1;
-            cnst2 -= q * cf2;
-            return i => new[] { cf1 * i + cnst1, cf2 * i + cnst2 };
-        }
+			// See which coefficient is largest and try to lower it maximally
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Inversion mod m. </summary>
-        ///
-        /// <remarks>   Inverts n mod a particular value.
-        /// Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="n">    The number to be inverted. </param>
-        /// <param name="mod">  The modulus. </param>
-        ///
-        /// <returns>  1/n Mod mod or null if there's no solution </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+			// If it's the first coefficient
+			if (TypeAdaptation.Abs(cnst1) > TypeAdaptation.Abs(cnst2))
+			{
+				// Get the multiplier for the first coefficient
+				q = cnst1 / cf1;
+			}
+			else
+			{
+				// Get the multiplier for the second coefficient
+				q = cnst2 / cf2;
+			}
+			// Lower both coefficients by the multiplier
+			cnst1 -= q * cf1;
+			cnst2 -= q * cf2;
 
-        public static nt InverseMod(this nt n, nt mod)
-        {
-            var solns = LinearCongruenceSolve(n, 1, mod);
-            if (solns == null)
-            {
-                return -1;
-            }
-            return solns[0];
-        }
+			// Return a function which returns values which solve the equation
+			return i => new[] { cf1 * i + cnst1, cf2 * i + cnst2 };
+		}
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>  Solves Mod(a*x, mod) == b for x </summary>
-        ///
-        /// <remarks>   Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="a">    The value of a in Mod(a*x, mod) == b. </param>
-        /// <param name="b">    The value of b in Mod(a*x, mod) == b. </param>
-        /// <param name="mod">  The modulus. </param>
-        ///
-        /// <returns>   x between 0 and mod-1 such that Mod(a*x, mod) == b or null if there's no solutions. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Inversion mod m. </summary>
+		///
+		/// <remarks>   Inverts n mod a particular value.
+		/// Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="n">    The number to be inverted. </param>
+		/// <param name="mod">  The modulus. </param>
+		///
+		/// <returns>  1/n Mod mod or null if there's no solution </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static nt[] LinearCongruenceSolve(nt a, nt b, nt mod)
-        {
-            nt gcd;
-            var fnSolns = DiophantineSolve(a, mod, b, out gcd);
-            if (fnSolns == null || gcd > int.MaxValue)
-            {
-                return null;
-            }
+		public static nt InverseMod(this nt n, nt mod)
+		{
+			var solns = LinearCongruenceSolve(n, 1, mod);
+			if (solns == null)
+			{
+				return -1;
+			}
+			return solns[0];
+		}
 
-            var ret = new nt[(int)gcd];
-            for (var i = 0; i < gcd; i++)
-            {
-                var sln = fnSolns(i);
-                ret[i] = sln[0];
-                if (ret[i] > mod)
-                {
-                    ret[i] -= (ret[i]/mod)*mod;
-                }
-                else if (ret[i] < 0)
-                {
-                    ret[i] += ((mod - 1 - ret[i])/mod)*mod;
-                }
-            }
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>  Solves Mod(a*x, mod) == b for x </summary>
+		///
+		/// <remarks>   Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="a">    The value of a in Mod(a*x, mod) == b. </param>
+		/// <param name="b">    The value of b in Mod(a*x, mod) == b. </param>
+		/// <param name="mod">  The modulus. </param>
+		///
+		/// <returns>   x between 0 and mod-1 such that Mod(a*x, mod) == b or null if there's no solutions. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            return ret;
-        }
-    }
+		public static nt[] LinearCongruenceSolve(nt a, nt b, nt mod)
+		{
+			nt gcd;
+			var fnSolns = DiophantineSolve(a, mod, b, out gcd);
+			if (fnSolns == null || gcd > int.MaxValue)
+			{
+				return null;
+			}
+
+			var ret = new nt[(int)gcd];
+			for (var i = 0; i < gcd; i++)
+			{
+				var sln = fnSolns(i);
+				ret[i] = sln[0];
+				if (ret[i] > mod)
+				{
+					ret[i] -= (ret[i]/mod)*mod;
+				}
+				else if (ret[i] < 0)
+				{
+					ret[i] += ((mod - 1 - ret[i])/mod)*mod;
+				}
+			}
+
+			return ret;
+		}
+	}
 }
--- NumberTheory/EuclideanExt.cs
+++ NumberTheory/EuclideanExt.cs
@@ -11,67 +11,74 @@ namespace NumberTheoryBig
 namespace NumberTheoryLong
 #endif
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// <summary>   Extended Euclidean algorithm. </summary>
-    ///
-    /// <remarks>   This is used to return the results from the extended Euclidean algorithm as given in
-    /// Computational Number Theory by Wagon and Bressoud.  See Euclidean.EuclideanExt for the function which
-    /// returns an object of this class.  I made a small attempt to try to keep the returned coefficients
-    /// relatively small.
-    /// Darrellp, 2/12/2011. </remarks>
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>   Extended Euclidean algorithm. </summary>
+	///
+	/// <remarks>   This is used to return the results from the extended Euclidean algorithm as given in
+	/// Computational Number Theory by Wagon and Bressoud.  See Euclidean.EuclideanExt for the function which
+	/// returns an object of this class.  I made a small attempt to try to keep the returned coefficients
+	/// relatively small.
+	/// Darrellp, 2/12/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public class EuclideanExt
-    {
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Gets the gcd after a call to Euclidean.EuclideanExt. </summary>
-        ///
-        /// <value> The gcd. </value>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+	public class EuclideanExt
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Gets the gcd after a call to Euclidean.EuclideanExt. </summary>
+		///
+		/// <value> The gcd. </value>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public nt GCD { get; private set; }
+		public nt GCD { get; private set; }
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Gets the coefficient for the first value in a call to Euclidean.EuclideanExt. </summary>
-        ///
-        /// <value> The coefficient. </value>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Gets the coefficient for the first value in a call to Euclidean.EuclideanExt. </summary>
+		///
+		/// <value> The coefficient. </value>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public nt Coeff1 { get; private set; }
+		public nt Coeff1 { get; private set; }
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Gets the coefficient for the second value in a call to Euclidean.EuclideanExt. </summary>
-        ///
-        /// <value> The coefficient. </value>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Gets the coefficient for the second value in a call to Euclidean.EuclideanExt. </summary>
+		///
+		/// <value> The coefficient. </value>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public nt Coeff2 { get; private set; }
+		public nt Coeff2 { get; private set; }
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Constructor based on the extended Euclidean Algorithm given in
-        /// Computation Number Theory by Wagon and Bressoud. </summary>
-        ///
-        /// <remarks>   Darrellp, 2/13/2011. </remarks>
-        ///
-        /// <param name="val1"> The first value. </param>
-        /// <param name="val2"> The second value. </param>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>   Constructor based on the extended Euclidean Algorithm given in
+		/// Computation Number Theory by Wagon and Bressoud. </summary>
+		///
+		/// <remarks>   Darrellp, 2/13/2011. </remarks>
+		///
+		/// <param name="val1"> The first value. </param>
+		/// <param name="val2"> The second value. </param>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        internal EuclideanExt(nt val1, nt val2)
-        {
-            var rst = new[] { new[] { TypeAdaptation.Abs(val1), 1, 0 }, new[] { TypeAdaptation.Abs(val2), 0, 1 } };
+		internal EuclideanExt(nt val1, nt val2)
+		{
+			// Set up our matrix
+			var rst = new[] { new[] { TypeAdaptation.Abs(val1), 1, 0 }, new[] { TypeAdaptation.Abs(val2), 0, 1 } };
 
-            while (rst[1][0] != 0)
-            {
-                var q = rst[0][0]/rst[1][0];
-                var rst1Save = rst[1];
-                rst[1] = rst[0].Zip(rst[1], (v1, v2) => v1 - q*v2).ToArray();
-                rst[0] = rst1Save;
-            }
-            GCD = rst[0][0];
-            Coeff1 = rst[0][1];
-            Coeff2 = rst[0][2];
-        }
+			// While our remainder is non-zero
+			while (rst[1][0] != 0)
+			{
+				// Figure out the current quotient
+				var q = rst[0][0]/rst[1][0];
+				var rst1Save = rst[1];
 
-    }
+				// Do the matrix multiply
+				rst[1] = rst[0].Zip(rst[1], (v1, v2) => v1 - q*v2).ToArray();
+				rst[0] = rst1Save;
+			}
+
+			// Pull the results we want out of the matrix
+			GCD = rst[0][0];
+			Coeff1 = rst[0][1];
+			Coeff2 = rst[0][2];
+		}
+
+	}
 }
