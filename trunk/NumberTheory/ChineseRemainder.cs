be56b448e9cca4046d7be3accc74ea07b90584d0
darrellp <darrellp@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2011-02-21 06:09:07 +0000

    tabification and comments

--- trunk/NumberTheory/ChineseRemainder.cs
+++ trunk/NumberTheory/ChineseRemainder.cs
@@ -12,39 +12,45 @@ namespace NumberTheoryBig
 namespace NumberTheoryLong
 #endif
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// <summary>   Static class to hold functions related to the Chinese remainder. </summary>
-    ///
-    /// <remarks>   Darrellp, 2/12/2011. </remarks>
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>   Static class to hold functions related to the Chinese remainder. </summary>
+	///
+	/// <remarks>   Darrellp, 2/12/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static class ChineseRemainder
-    {
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
-        /// <summary>   Implements the Chinese Remainder Theorem.  This version of the algorithm
-        /// only works for pairwise coprime moduli. This algorithm solves for x a set of simultaneous
-        /// congruencies of the form x % mods[k] == a[k] where mods and a are parameters to the function.</summary>
-        ///
-        /// <remarks>   Darrellp, 2/12/2011. </remarks>
-        ///
-        /// <param name="aVals">    Array of values for the a[k] in the equation above. </param>
-        /// <param name="mods">     The mods in the equation above. </param>
-        ///
-        /// <returns>   A value which satisfies all the above congruencies. </returns>
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static class ChineseRemainder
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	
+		/// Implements the Chinese Remainder Theorem.  This version of the algorithm only works for
+		/// pairwise coprime moduli. This algorithm solves for x a set of simultaneous congruencies of
+		/// the form x % mods[k] == a[k] where mods and a are parameters to the function. 
+		/// </summary>
+		///
+		/// <remarks>	Darrellp, 2/12/2011. </remarks>
+		///
+		/// <param name="aVals">	Array of values for the a[k] in the equation above. </param>
+		/// <param name="mods">		The mods in the equation above. </param>
+		///
+		/// <returns>	A value which satisfies all the above congruencies. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public static nt CRT(nt[] aVals, nt[] mods)
-        { 
-            var mult = mods.Aggregate((acc, next) => acc * next);
+		public static nt CRT(nt[] aVals, nt[] mods)
+		{ 
+			// Ensure that the moduli are pairwise relatively prime
+			var mult = mods.Aggregate((acc, next) => acc * next);
 
-            if (mult != Euclidean.LCM(mods))
-            {
-                return -1;
-            }
-            return mods.
-                Select(m => (mult/m).InverseMod(m) * mult / m).
-                Zip(aVals, (m, a) => a * m).
-                Aggregate((acc,val) => acc + val) % mult;
-        }
-    }
+			// If they're not relatively prime
+			if (mult != Euclidean.LCM(mods))
+			{
+				return -1;
+			}
+
+			// If they are, proceed with the CRT
+			return mods.
+				Select(m => (mult/m).InverseMod(m) * mult / m).
+				Zip(aVals, (m, a) => a * m).
+				Aggregate((acc,val) => acc + val) % mult;
+		}
+	}
 }
