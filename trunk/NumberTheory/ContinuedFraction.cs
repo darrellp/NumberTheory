007833ea7b7ee4f4ca52f2684afe07b388316e96
Rain <Rain@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2013-08-15 23:27:47 +0000

    Changed ContinuedFraction to be a class, added operators, equality and convergents.  Changed tests accordingly also.

--- trunk/NumberTheory/ContinuedFraction.cs
+++ trunk/NumberTheory/ContinuedFraction.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 #if BIGINTEGER
 using nt = System.Numerics.BigInteger;
 #elif LONG
@@ -10,16 +8,20 @@ using nt = System.Int64;
 
 #if BIGINTEGER
 
+// ReSharper disable once CheckNamespace
 namespace NumberTheoryBig
 #elif LONG
 namespace NumberTheoryLong
 #endif
 {
 	/// <summary>
-	/// Solve Pell's equation through continued fractions
+	/// Represents a continued fraction
 	/// </summary>
-	public static class Cfr
+	public class ContinuedFraction
 	{
+		public Rational Val { get; private set; }
+		public List<nt> Vals {get; private set;}
+
 		/// <summary>
 		/// Returns the continued fraction form for a/b.  The first value of 
 		/// the list will be 0 if a is less than b.  
@@ -27,26 +29,52 @@ namespace NumberTheoryLong
 		/// <param name="a">numerator</param>
 		/// <param name="b">denonimator</param>
 		/// <returns>List of the continued fraction values for a/b</returns>
-		static public List<nt> ContinuedFraction(nt a, nt b)
+		public ContinuedFraction(nt a, nt b)
 		{
-			var ret = new List<nt>();
-
+			Vals = new List<nt>();
+			Val = new Rational(a, b);
 			while (true)
 			{
-				ret.Add(a / b);
+				Vals.Add(a / b);
 				var oldB = b;
 				b = a % b;
 				if (b == 0)
 				{
-					return ret;
+					return;
 				}
 				a = oldB;
 			}
 		}
 
-		static public nt Convergents(List<nt> cf)
+		public ContinuedFraction(Rational r) : this(r.Num, r.Den){}
+
+		public List<Rational> Convergents()
 		{
-			return -1;
+			if (Vals.Count == 1)
+			{
+				return new List<Rational> {(Rational)Vals[0]};
+			}
+			var ret = new List<Rational>();
+			var pm2 = Vals[0];
+			var qm2 = (nt)1;
+			var pm1 = Vals[0] * Vals[1] + 1;
+			var qm1 = Vals[1];
+
+			ret.Add(new Rational(pm2, qm2));
+			ret.Add(new Rational(pm1, qm1));
+
+			for (var iVal = 2; iVal < Vals.Count; iVal++)
+			{
+				var pCur = Vals[iVal] * pm1 + pm2;
+				var qCur = Vals[iVal] * qm1 + qm2;
+				ret.Add(new Rational(pCur, qCur));
+
+				qm2 = qm1;
+				pm2 = pm1;
+				qm1 = qCur;
+				pm1 = pCur;
+			}
+			return ret;
 		}
 	}
 }
