655a7ad5494193890f3bbd8ab23ccfc1de0c4948
Rain <Rain@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2013-08-15 07:23:37 +0000

    Added support for rationals

---
+++ trunk/NumberTheory/ContinuedFraction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if BIGINTEGER
+using nt = System.Numerics.BigInteger;
+#elif LONG
+using nt = System.Int64;
+#endif
+
+
+#if BIGINTEGER
+
+namespace NumberTheoryBig
+#elif LONG
+namespace NumberTheoryLong
+#endif
+{
+	/// <summary>
+	/// Solve Pell's equation through continued fractions
+	/// </summary>
+	public static class Cfr
+	{
+		/// <summary>
+		/// Returns the continued fraction form for a/b.  The first value of 
+		/// the list will be 0 if a is less than b.  
+		/// </summary>
+		/// <param name="a">numerator</param>
+		/// <param name="b">denonimator</param>
+		/// <returns>List of the continued fraction values for a/b</returns>
+		static public List<nt> ContinuedFraction(nt a, nt b)
+		{
+			var ret = new List<nt>();
+
+			while (true)
+			{
+				ret.Add(a / b);
+				var oldB = b;
+				b = a % b;
+				if (b == 0)
+				{
+					return ret;
+				}
+				a = oldB;
+			}
+		}
+
+		static public nt Convergents(List<nt> cf)
+		{
+			return -1;
+		}
+	}
+}
