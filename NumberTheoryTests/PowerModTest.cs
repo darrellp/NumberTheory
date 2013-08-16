f265021b12981ec51065b9ef5b49ef4ac44e983c
Rain <Rain@44d09f27-2ef3-784d-b9ad-ccd84d14832b>
2013-08-16 08:31:36 +0000

    Added MatrixPower in prep for Pell's

--- NumberTheoryTests/PowerModTest.cs
+++ NumberTheoryTests/PowerModTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #if BIGINTEGER
 using NumberTheoryBig;
@@ -17,5 +18,23 @@ namespace NumberTheoryTests
             Assert.AreEqual(3115, PowerMod.Power(2357, 2357, 3599));
             Assert.AreEqual(5,PowerMod.Power(7,-1,17));
         }
+
+	    [TestMethod]
+	    public void MatrixPowerTest()
+	    {
+		    var mtx = new BigInteger[]
+		    {
+			    new BigInteger(18),
+				new BigInteger(5), 
+				new BigInteger(65), 
+				new BigInteger(18)
+		    };
+
+		    var res = PowerMod.MatrixPower(5, mtx);
+		    Assert.AreEqual(30349818, res[0]);
+			Assert.AreEqual(8417525, res[1]);
+			Assert.AreEqual(109427825, res[2]);
+			Assert.AreEqual(30349818, res[3]);
+		}
     }
 }
